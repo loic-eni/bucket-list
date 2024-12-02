b9e035c2711649c0a8a68e7188f87344e31ef6ac
lduvail2022 <loic.duvail2022@campus-eni.fr>
2024-12-02 14:09:34 +0100

    module6 tp: wish create and update

---
+++ src/Form/WishType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description',null, ['required'=>false])
+            ->add('author')
+            ->add("addWish", SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+        ]);
+    }
+}
